35cc2a55e95c475fe8897d6e030181a9a4ffb531
Hardman1festo <hardman1festo@gmail.com>
2018-11-30 20:32:40 +0200

    It works

---
+++ app/scripts/main.js
@@ -0,0 +1,34 @@
+$(document).ready(function () {
+  var longUrl = '';
+
+  function getTinyUrl(longUrl) {
+  $.ajax({
+    url: 'https://www.googleapis.com/urlshortener/v1/url?key=AIzaSyBPmtkNc4pJxkQAFXrgIpPN1GY5i-h1wP4',
+    type: 'POST',
+    contentType: 'application/json; charset=utf-8',
+    data: '{ longUrl: "' + longUrl +'"}',
+    success: function(response) {
+      var tinyUrl = response.id;
+      console.log(tinyUrl);
+      $('#tiny-url').text(tinyUrl).attr('href', tinyUrl);
+      $('#hided-button').css('opacity', 1);
+    }
+  });
+  };
+
+    $('form').on('submit', function(event) {
+    event.preventDefault();
+    longUrl = ($(this).serializeArray()[0].value);
+    console.log(longUrl);
+    getTinyUrl(longUrl);
+    });
+
+  $('#hided-button').click(function () {
+    var $temp = $('<input>');
+    $('body').append($temp);
+    $temp.val($('#tiny-url').text()).select();
+    document.execCommand('copy');
+    $temp.remove();
+  });
+});
+
